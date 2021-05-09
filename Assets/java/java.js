16a4d5957d1242cc5aa6682503ec8e546e943b64
Regina Franco <79554515+reginaafc@users.noreply.github.com>
2021-05-08 23:20:36 -0500

    Add files via upload

---
+++ Assets/java/java.js
@@ -0,0 +1,162 @@
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var navcontent = document.getElementById("nav-content");
+var navaction = document.getElementById("navAction");
+var brandname = document.getElementById("brandname");
+var toToggle = document.querySelectorAll(".toggleColour");
+
+document.addEventListener("scroll", function () {
+  /*Apply classes for slide in bar*/
+  scrollpos = window.scrollY;
+
+  if (scrollpos > 10) {
+    header.classList.add("bg-white");
+    navaction.classList.remove("bg-white");
+    navaction.classList.add("gradient");
+    navaction.classList.remove("text-gray-800");
+    navaction.classList.add("text-white");
+    //Use to switch toggleColour colours
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("text-gray-800");
+      toToggle[i].classList.remove("text-white");
+    }
+    header.classList.add("shadow");
+    navcontent.classList.remove("bg-gray-100");
+    navcontent.classList.add("bg-white");
+  } else {
+    header.classList.remove("bg-white");
+    navaction.classList.remove("gradient");
+    navaction.classList.add("bg-white");
+    navaction.classList.remove("text-white");
+    navaction.classList.add("text-gray-800");
+    //Use to switch toggleColour colours
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("text-white");
+      toToggle[i].classList.remove("text-gray-800");
+    }
+
+    header.classList.remove("shadow");
+    navcontent.classList.remove("bg-white");
+    navcontent.classList.add("bg-gray-100");
+  }
+});
+
+/*Toggle dropdown list*/
+/*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
+
+var navMenuDiv = document.getElementById("nav-content");
+var navMenu = document.getElementById("nav-toggle");
+
+document.onclick = check;
+function check(e) {
+  var target = (e && e.target) || (event && event.srcElement);
+
+  //Nav Menu
+  if (!checkParent(target, navMenuDiv)) {
+    // click NOT on the menu
+    if (checkParent(target, navMenu)) {
+      // click on the link
+      if (navMenuDiv.classList.contains("hidden")) {
+        navMenuDiv.classList.remove("hidden");
+      } else {
+        navMenuDiv.classList.add("hidden");
+      }
+    } else {
+      // click both outside link and outside menu, hide menu
+      navMenuDiv.classList.add("hidden");
+    }
+  }
+}
+function checkParent(t, elm) {
+  while (t.parentNode) {
+    if (t == elm) {
+      return true;
+    }
+    t = t.parentNode;
+  }
+  return false;
+}
+
+var card = document.querySelector(".card");
+card.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
+
+
+// This is for the work tabs 
+
+
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+ 
+ 	//Parallax            
+              
+           
+
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }              
+              
+  
+//Scroll back to top
+              
+$(document).ready(function() {	
+		
+  		
+		/* Hero Case study images */			
+		
+		$('.case-study-name:nth-child(1)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(1)').addClass("show");
+			$('.case-study-name:nth-child(1)').addClass('active');
+		})
+		$('.case-study-name:nth-child(2)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(2)').addClass("show");
+			$('.case-study-name:nth-child(2)').addClass('active');
+		})
+		$('.case-study-name:nth-child(3)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(3)').addClass("show");
+			$('.case-study-name:nth-child(3)').addClass('active');
+		})
+		$('.case-study-name:nth-child(4)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(4)').addClass("show");
+			$('.case-study-name:nth-child(4)').addClass('active');
+		})
+		$('.case-study-name:nth-child(1)').trigger('mouseenter')
+  
+  });            
+              
+})(jQuery); 
